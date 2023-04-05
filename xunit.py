dda98e6a1f033b3060b99433c12422de2b7aced4
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-05 15:16:52 -0300

    Implementada a classe WasRun

--- xunit.py
+++ xunit.py
@@ -1,3 +1,12 @@
+class WasRun:
+    wasRun: int | None
+    def __init__(self, name: str):
+        self.wasRun = None
+
+    def testMethod(self) -> None:
+        self.wasRun = 1
+
+
 def main() -> None:
     test = WasRun("testMethod")
     print(test.wasRun)
