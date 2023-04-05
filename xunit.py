76ff9593cefcc1078435bde7c4395f4cc904b835
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-05 18:46:39 -0300

    Criada superclasse TestCase. Movidos os atributos name e run para a TestCase

--- xunit.py
+++ xunit.py
@@ -1,17 +1,22 @@
-class WasRun:
-    wasRun: int | None
+class TestCase:
     name: str
-    
     def __init__(self, name: str):
-        self.wasRun = None
         self.name = name
-
-    def testMethod(self) -> None:
-       self.wasRun = 1
 
     def run(self) -> None:
         method = getattr(self, self.name)
         method()
+        
+
+class WasRun(TestCase):
+    wasRun: int | None
+    
+    def __init__(self, name: str):
+        self.wasRun = None
+        super().__init__(name)
+
+    def testMethod(self) -> None:
+       self.wasRun = 1
 
 
 def main() -> None:
