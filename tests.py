8f11d8012801f6a651ebb4c53f21c5bd4bb36049
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-06 09:53:06 -0300

    Extraída a criação do result de cada método de teste para a setUp da TestCaseTest

--- tests.py
+++ tests.py
@@ -3,32 +3,32 @@ from xunit import TestCase, WasRun, TestResult, TestSuite
 
 class TestCaseTest(TestCase):
     test: WasRun
+    result: TestResult
+    
+    def setUp(self) -> None:
+        self.result = TestResult()
 
     def testTemplateMethod(self) -> None:
-        result = TestResult()
         self.test = WasRun("testMethod")
-        self.test.run(result)
+        self.test.run(self.result)
         assert self.test.log == "setUp testMethod tearDown"
 
     def testResult(self) -> None:
-        result = TestResult()
         test = WasRun("testMethod")
-        test.run(result)
-        assert "1 run, 0 failed" == result.summary()
+        test.run(self.result)
+        assert "1 run, 0 failed" == self.result.summary()
 
     def testFailedResult(self) -> None:
-        result = TestResult()
         test = WasRun("testBrokenMethod")
-        test.run(result)
-        assert "1 run, 1 failed" == result.summary()
+        test.run(self.result)
+        assert "1 run, 1 failed" == self.result.summary()
 
     def testSuite(self) -> None:
         suite = TestSuite()
         suite.add(WasRun("testMethod"))
         suite.add(WasRun("testBrokenMethod"))
-        result = TestResult()
-        suite.run(result)
-        assert "2 run, 1 failed" == result.summary()
+        suite.run(self.result)
+        assert "2 run, 1 failed" == self.result.summary()
 
 
 def main() -> None:
