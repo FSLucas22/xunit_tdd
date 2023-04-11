ee86955e0adf3128a3acefe133e6e32e25540d0d
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-11 20:37:15 -0300

    Modificada a função Test para atender à sintaxe de decorador

--- xunit/src/testdecorator.py
+++ xunit/src/testdecorator.py
@@ -3,14 +3,16 @@ from xunit.src.testcase import TestCase
 
 
 TestSubCase = TypeVar('TestSubCase', bound=TestCase)
+TestMethod = Callable[[TestSubCase], None]
 
-
-def Test(test_cls: Type[TestCase],
-         test_method: Callable[[TestSubCase], None],
-         ) -> Callable[[TestSubCase], None]:
-    test_name = test_method.__name__
-    if not hasattr(test_cls, 'testNames') or test_cls.testNames == "":
-        test_cls.testNames = test_name
+def Test(test_cls: Type[TestCase]
+         ) -> Callable[[TestMethod[TestSubCase]], TestMethod[TestSubCase]]:
+    def getMethod(test_method: TestMethod[TestSubCase]
+                  ) -> TestMethod[TestSubCase]:
+        test_name = test_method.__name__
+        if not hasattr(test_cls, 'testNames') or test_cls.testNames == "":
+            test_cls.testNames = test_name
+            return test_method
+        test_cls.testNames += " " + test_name
         return test_method
-    test_cls.testNames += " " + test_name
-    return test_method
+    return getMethod
