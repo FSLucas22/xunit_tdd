9a21916d538732593822213c0b193c1b3752f8c9
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-11 16:32:32 -0300

    Refatoração para satisfazer o mypy --strict

--- xunit/tests/__init__.py
+++ xunit/tests/__init__.py
@@ -1,5 +1,5 @@
 from xunit.src import *
-from xunit.tests.test_testsummary import TestSummaryTest
-from xunit.tests.test_testresult import TestResultTest
-from xunit.tests.test_testcase import TestCaseTest
-from xunit.tests.test_testsuite import TestSuiteTest
+from xunit.tests.test_testsummary import TestSummaryTest as TestSummaryTest
+from xunit.tests.test_testresult import TestResultTest as TestResultTest
+from xunit.tests.test_testcase import TestCaseTest as TestCaseTest
+from xunit.tests.test_testsuite import TestSuiteTest as TestSuiteTest
