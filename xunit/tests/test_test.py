8f29b9701340e2db814b03dfbc5a8ab615cfebac
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-11 19:39:06 -0300

    Incrementadas verificações em testNameIsAddedByDecorator

--- xunit/tests/test_test.py
+++ xunit/tests/test_test.py
@@ -7,7 +7,13 @@ class TestTest(TestCase):
     
     def testNameIsAddedByDecorator(self) -> None:
         assert not hasattr(UnnamedTestClass, "testNames")
+        result_before_decorator = TestResult()
+        result_after_decorator = TestResult()
+        UnnamedTestClass("testMethod").run(result_before_decorator)
         setattr(UnnamedTestClass, "testMethod", Test(
             UnnamedTestClass, UnnamedTestClass.testMethod 
         ))
         assert UnnamedTestClass.testNames == "testMethod"
+        UnnamedTestClass("testMethod").run(result_after_decorator)
+        assert result_before_decorator.getAllPassed() ==\
+               result_after_decorator.getAllPassed()
