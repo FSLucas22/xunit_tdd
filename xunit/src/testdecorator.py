2eb1c619cd90181f7cfe02d1ef0785955dfc2457
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-11 19:25:13 -0300

    Criada implementação inicial de automatização de adição automatica de nomes

--- xunit/src/testdecorator.py
+++ xunit/src/testdecorator.py
@@ -7,4 +7,8 @@ TestSubCase = TypeVar('TestSubCase', bound=TestCase)
 
 def Test(test_method: Callable[[TestSubCase], None],
          test_cls: Type[TestCase]) -> None:
-    pass
+    test_name = test_method.__name__
+    if not hasattr(test_cls, 'testNames') or test_cls.testNames == "":
+        test_cls.testNames = test_name
+        return
+    test_cls.testNames += " " + test_name
