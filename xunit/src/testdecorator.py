6aa6e8855df4cadd18a4c254d8c9e35e96f1efb0
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-11 20:48:25 -0300

    Modificado o decorador para atender ao novo teste

--- xunit/src/testdecorator.py
+++ xunit/src/testdecorator.py
@@ -5,14 +5,5 @@ from xunit.src.testcase import TestCase
 TestSubCase = TypeVar('TestSubCase', bound=TestCase)
 TestMethod = Callable[[TestSubCase], None]
 
-def Test(test_cls: Type[TestCase]
-         ) -> Callable[[TestMethod[TestSubCase]], TestMethod[TestSubCase]]:
-    def getMethod(test_method: TestMethod[TestSubCase]
-                  ) -> TestMethod[TestSubCase]:
-        test_name = test_method.__name__
-        if not hasattr(test_cls, 'testNames') or test_cls.testNames == "":
-            test_cls.testNames = test_name
-            return test_method
-        test_cls.testNames += " " + test_name
-        return test_method
-    return getMethod
+def Test(test_method: TestMethod[TestSubCase]) -> TestMethod[TestSubCase]:
+    return test_method
