8cfd23feec076e6ac7a5c7b469400772eed42c4b
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-13 19:34:40 -0300

    Modificada a exception levantada pelo método de teste

--- xunit/tests/test_errors.py
+++ xunit/tests/test_errors.py
@@ -7,8 +7,9 @@ class TestErrors(TestCase):
     @Test
     def testErrorInfo(self) -> None:
         try:
-            raise Exception
+            raise InvalidAttributeException
         except Exception as e:
             info = TestErrorInfo(e)
             assert info.line_number == 10
-            assert info.exception_type == Exception
+            assert info.exception_type == InvalidAttributeException
+            
