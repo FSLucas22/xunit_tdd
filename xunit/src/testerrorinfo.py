69931fb23071e8c75f4194e09b40eee9be39a474
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-13 20:01:18 -0300

    Implementada funcionalidade de error_info

--- xunit/src/testerrorinfo.py
+++ xunit/src/testerrorinfo.py
@@ -1,15 +1,16 @@
 import traceback
 from typing import Type
 
+
 class TestErrorInfo:
     line_number: int
     exception_type: Type[Exception]
     path: str
+    error_info: str
     
-    def __init__(self, error: Exception):
+    def __init__(self, error: Exception, error_info: str):
         info = traceback.extract_tb(error.__traceback__)[-1]
         self.line_number = info.lineno or 0
         self.exception_type = type(error)
         self.path = info.filename
-        print(''.join(traceback.format_exception(
-            type(error), error, error.__traceback__)))
+        self.error_info = error_info
--- xunit/tests/test_errors.py
+++ xunit/tests/test_errors.py
@@ -11,7 +11,7 @@ class TestErrors(TestCase):
             raise InvalidAttributeException
         except Exception as e:
             info = TestErrorInfo(e, error_info="Test")
-            assert info.line_number == 10
+            assert info.line_number == 11
             assert info.exception_type == InvalidAttributeException
             assert info.path == __file__
             assert info.error_info == "Test"
