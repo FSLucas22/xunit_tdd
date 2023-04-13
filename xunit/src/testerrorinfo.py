dfac191e2ea3b0acb316cc299c1c40ccd6c96f29
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-13 19:45:13 -0300

    Implementada path

--- xunit/src/testerrorinfo.py
+++ xunit/src/testerrorinfo.py
@@ -4,8 +4,10 @@ from typing import Type
 class TestErrorInfo:
     line_number: int
     exception_type: Type[Exception]
+    path: str
     
     def __init__(self, error: Exception):
         info = traceback.extract_tb(error.__traceback__)[-1]
         self.line_number = info.lineno or 0
         self.exception_type = type(error)
+        self.path = info.filename
