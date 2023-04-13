96fc121a27ca5892123c0f67b2a4a189346af566
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-13 19:32:41 -0300

    Implementada stub de exception_type

--- xunit/src/testerrorinfo.py
+++ xunit/src/testerrorinfo.py
@@ -1,9 +1,11 @@
 import traceback
-
+from typing import Type
 
 class TestErrorInfo:
     line_number: int
-
+    exception_type: Type[Exception]
+    
     def __init__(self, error: Exception):
         info = traceback.extract_tb(error.__traceback__)[-1]
         self.line_number = info.lineno or 0
+        self.exception_type = Exception
