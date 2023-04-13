2e15c66e5a2df40ac9394d23c0b1abae73347bff
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-13 19:29:10 -0300

    Implementada classe TestErrorInfo

--- xunit/src/testerrorinfo.py
+++ xunit/src/testerrorinfo.py
@@ -1,5 +1,9 @@
+import traceback
+
+
 class TestErrorInfo:
     line_number: int
 
     def __init__(self, error: Exception):
-        self.line_number = 10
+        info = traceback.extract_tb(error.__traceback__)[-1]
+        self.line_number = info.lineno or 0
