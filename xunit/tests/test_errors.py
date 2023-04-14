49a2e1e797d7d3f6bdae968efb844dafc7d6c9be
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-14 12:32:42 -0300

    Removido teste por se tornar obsoleto

--- xunit/tests/test_errors.py
+++ xunit/tests/test_errors.py
@@ -17,11 +17,3 @@ class TestErrors(TestCase):
             assert info.path == __file__
             assert info.error_info == "Test"
             assert info.test_name == "testErrorInfo"
-
-    @Test
-    def testResultAcceptsErroInfo(self) -> None:
-        e = Exception()
-        info = TestErrorInfo(e, 11, "Test", "testResultAcceptsErroInfo")
-        result = TestResult()
-        result.testFailed("testMethod", info)
-        result.testNotCompleted("testMethod", info)
