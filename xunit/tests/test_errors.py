0bd208cae584a3861c7d68c63af6403c069294d7
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-14 21:14:15 -0300

    Criado teste para implementação de método factory

--- xunit/tests/test_errors.py
+++ xunit/tests/test_errors.py
@@ -1,5 +1,4 @@
 from xunit.src import *
-import traceback
 
 
 @TestClass
@@ -13,6 +12,18 @@ class TestErrors(TestCase):
             info = TestErrorInfo(e, line_number=11, error_info="Test",
                                  test_name="testErrorInfo")
             assert info.line_number == 11
+            assert info.exception_type == InvalidAttributeException
+            assert info.path == __file__
+            assert info.error_info == "Test"
+            assert info.test_name == "testErrorInfo"
+
+    @Test
+    def testFromException(self) -> None:
+        try:
+            raise InvalidAttributeException("Test")
+        except Exceptin as e:
+            info = TestErrorInfo.fromException(e)
+            assert info.line_number == 23
             assert info.exception_type == InvalidAttributeException
             assert info.path == __file__
             assert info.error_info == "Test"
