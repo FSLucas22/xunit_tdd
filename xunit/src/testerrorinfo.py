7ae23b75cc4522b97354390390eca82488731525
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-15 11:32:51 -0300

    Implementado método para controlar o test_name

--- xunit/src/testerrorinfo.py
+++ xunit/src/testerrorinfo.py
@@ -22,13 +22,13 @@ class TestErrorInfo:
             self.path = None
 
     @staticmethod
-    def fromException(error: Exception) -> 'TestErrorInfo':
+    def fromException(error: Exception, test_name: str | None = None) -> 'TestErrorInfo':
         info = traceback.extract_tb(error.__traceback__)[-1]
         line_number = info.lineno or -1
         error_info = ''.join(
             traceback.format_exception(type(error), error, error.__traceback__)
         )
-        test_name = info[2]
+        test_name = test_name or info[2]
         return TestErrorInfo(error, line_number, error_info, test_name)
 
     def __str__(self) -> str:
