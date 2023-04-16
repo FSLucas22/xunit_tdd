b7fb6ae33e53f1239f6b8d54bc2cdb2e6a120ea7
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-16 10:53:12 -0300

    Incrementadas verificações

--- xunit/tests/test_colors.py
+++ xunit/tests/test_colors.py
@@ -7,3 +7,4 @@ class TestColors(TestCase):
     @Test
     def testColorFunctions(self) -> None:
         assert green("Test") == '\033[32mTest\033[0m'
+        assert red("Test") == '\033[31mTest\033[0m'
