b1b050e64e0079f8338f111040793d330a5a20e3
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-17 13:10:17 -0300

    Implementada stub do método

--- xunit/src/testexceptions.py
+++ xunit/src/testexceptions.py
@@ -1,9 +1,14 @@
 from typing import Type, Any
+from contextlib import contextmanager
 
 
 class InvalidAttributeException(Exception):
     pass
 
 
+@contextmanager
 def expects(error_class: Type[Exception]) -> Any:
-    ...
+    try:
+        yield
+    except Exception as e:
+        pass
