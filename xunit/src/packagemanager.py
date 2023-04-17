3f98533818d719ae75fc2aeb3a36f40af29c8be5
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-17 19:29:56 -0300

    Iniciada implementaçãod de getPackageObjects

--- xunit/src/packagemanager.py
+++ xunit/src/packagemanager.py
@@ -1,5 +1,7 @@
-from typing import NamedTuple
+from typing import NamedTuple, cast
 from types import ModuleType
+import pkgutil
+from os.path import dirname
 
 
 class PackageObject(NamedTuple):
@@ -7,5 +9,21 @@ class PackageObject(NamedTuple):
     value: ModuleType
 
 
+class InvalidPathError(Exception):
+    pass
+
+
 def getPackageObjects(package: ModuleType) -> list[PackageObject]:
-    return []
+    if not package.__file__:
+        raise InvalidPathError("package must have a valid path")
+    
+    package_path = dirname(package.__file__)
+    modules = pkgutil.iter_modules([package_path])
+    objects: list[PackageObject] = []
+    
+    for module in modules:
+        name = module.name
+        value = getattr(package, name)
+        objects.append(PackageObject(name, value))
+    print(objects)
+    return objects
