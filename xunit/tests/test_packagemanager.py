82d60acd48597bff867b080d38c1132ddde3dac0
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-17 19:51:05 -0300

    Incrementadas verificações

--- xunit/tests/test_packagemanager.py
+++ xunit/tests/test_packagemanager.py
@@ -13,10 +13,14 @@ class TestPackageManager(TestCase):
     @Test
     def testGetPackageObjects(self) -> None:
         from xunit.tests import testpackage
-        from xunit.tests.testpackage import packagemodule, packagemodule2
+        from xunit.tests.testpackage import (
+            packagemodule, packagemodule2, subpackage
+        )
         package_objects = getPackageObjects(testpackage)
         expected_object1 = PackageObject("packagemodule", packagemodule)
         expected_object2 = PackageObject("packagemodule2", packagemodule2)
+        expected_object3 = PackageObject("subpackage", subpackage, True)
         assert expected_object1 in package_objects
         assert expected_object2 in package_objects
-        assert len(getPackageObjects(testpackage)) == 2
+        assert expected_object3 in package_objects
+        assert len(package_objects) == 3
