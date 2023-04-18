8668d1c63d1c640b6ea60545094bebadf82f1bfc
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-18 20:12:25 -0300

    Criada função para permitir a visualização dos testes

--- test_xunit.py
+++ test_xunit.py
@@ -1,5 +1,7 @@
 import xunit
 import xunittest
+import colorama
+import os
 
 
 @xunit.TestClass
@@ -10,6 +12,26 @@ class TestXunit(xunit.TestCase):
         suite = xunit.TestSuite.fromModule(xunittest)
         result = xunit.TestResult()
         suite.run(result)
-        assert result.getAllPassed() == "testMethod testMethod2"
+        assert result.getAllPassed() == "testMethod"
         assert result.getAllFailed() == "failedMethod"
-        
+
+
+def main() -> None:
+    
+    result = xunit.TestResult()
+    summary = xunit.MixedTestSummary(
+        xunit.PassedSummary(passed_formatter=xunit.green),
+        xunit.ErrorInfoSummary(
+            failed_formatter=xunit.red, notCompleted_formatter=xunit.yellow),
+        xunit.SimpleTestSummary()
+    )
+    suite = xunit.TestSuite.fromTestCase(TestXunit)
+    suite.run(result)
+    print(summary.results(result))
+
+if __name__ == "__main__":
+    if os.name == "nt":
+        colorama.init()
+    main()
+    if os.name == "nt":
+        colorama.deinit()
