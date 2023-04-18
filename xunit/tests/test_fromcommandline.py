c63d0ad915788993af9a876098ecec9e46ef040d
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-18 18:46:36 -0300

    Incrementadas verificações

--- xunit/tests/test_fromcommandline.py
+++ xunit/tests/test_fromcommandline.py
@@ -13,4 +13,13 @@ class TestFromCommandLine(TestCase):
         )
         assert module.__name__ == testpackage.__name__
         assert module.__file__ == testpackage.__file__
-        
+
+        result1 = TestResult()
+        result2 = TestResult()
+        suite1 = TestSuite.fromPackage(testpackage)
+        suite2 = TestSuite.fromPackage(module)
+        suite1.run(result1)
+        suite2.run(result2)
+        assert result1.getAllPassed() == result2.getAllPassed()
+        assert result1.getAllFailed() == result2.getAllFailed()
+        assert result1.getAllNotCompleted() == result2.getAllNotCompleted()
