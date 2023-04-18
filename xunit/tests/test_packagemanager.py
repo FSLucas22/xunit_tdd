b504bccc611b021668d31d1f500dfee1bf3ece8a
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-17 22:38:35 -0300

    Iniciados testes para encontro de arquivos ignore.txt

--- xunit/tests/test_packagemanager.py
+++ xunit/tests/test_packagemanager.py
@@ -36,4 +36,9 @@ class TestPackageManager(TestCase):
         )
         assert len(package_objects) == 1
         assert expected_object == package_objects[0]
+
+    @Test
+    def getIgnoreFile(self) -> None:
+        from xunit.tests import testpackage
+        assert getIgnoreFile(testpackage) is None
         
