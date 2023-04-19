9d64416eb49d0c64e3769e4be46d853900afc317
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-19 17:35:00 -0300

    Removidas asserções para permitir a remoção de atributos da classe

--- xunit/tests/test_errors.py
+++ xunit/tests/test_errors.py
@@ -13,9 +13,6 @@ class TestErrors(TestCase):
         except Exception as e:
             info = TestErrorInfo(e, line_number=12, error_info="Test",
                                  test_name="testErrorInfo")
-            assert info.line_number == 12
-            assert info.exception_type == InvalidAttributeException
-            assert info.path == __file__
             assert info.error_info == "Test"
             assert info.test_name == "testErrorInfo"
 
@@ -26,9 +23,6 @@ class TestErrors(TestCase):
         except Exception as e:
             error_info = traceback.extract_tb(e.__traceback__)[-1]
             info = TestErrorInfo.fromException(e)
-            assert info.line_number == 25
-            assert info.exception_type == InvalidAttributeException
-            assert info.path == __file__
             assert info.error_info == ''.join(
             traceback.format_exception(type(e), e, e.__traceback__)
             )
