f4de89449b037ab5fc1c87ef3d48bce6771cc79e
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-19 17:38:25 -0300

    Removidos registros dos parametros a serem excluidos

--- xunit/src/testerrorinfo.py
+++ xunit/src/testerrorinfo.py
@@ -11,28 +11,20 @@ class TestErrorInfo:
     
     def __init__(self, error: Exception, line_number: int,
                  error_info: str, test_name: str):
-        self.line_number = line_number
         self.error_info = error_info
-        self.exception_type = type(error)
         self.test_name = test_name
-        try:
-            info = traceback.extract_tb(error.__traceback__)[-1]
-            self.path = info.filename
-        except:
-            self.path = None
 
     @staticmethod
     def fromException(error: Exception, test_name: str | None = None) -> 'TestErrorInfo':
-        info = traceback.extract_tb(error.__traceback__)[-1]
-        line_number = info.lineno or -1
+        name = traceback.extract_tb(error.__traceback__)[-1][2]
         error_info = ''.join(
             traceback.format_exception(type(error), error, error.__traceback__)
         )
-        test_name = test_name or info[2]
-        return TestErrorInfo(error, line_number, error_info, test_name)
+        test_name = test_name or name
+        return TestErrorInfo(error, 1, error_info, test_name)
 
     def __str__(self) -> str:
-        return f"TestErrorInfo({self.line_number}, {self.test_name}, {self.error_info})"
+        return f"TestErrorInfo({self.test_name}, {self.error_info})"
 
 
 class ErrorInfoFactory(Protocol):
