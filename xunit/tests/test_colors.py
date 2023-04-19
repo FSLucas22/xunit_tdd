9f3d7c1c4ec4a78e03bc7ef667f37d26e055f49d
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-19 20:11:07 -0300

    Adaptados nomes para snake case

--- xunit/tests/test_colors.py
+++ xunit/tests/test_colors.py
@@ -5,7 +5,7 @@ from xunit.src import *
 class TestColors(TestCase):
 
     @Test
-    def testColorFunctions(self) -> None:
+    def test_color_functions(self) -> None:
         assert color.green("Test") == '\033[32mTest\033[0m'
         assert color.red("Test") == '\033[31mTest\033[0m'
         assert color.yellow("Test") == '\033[33mTest\033[0m'
