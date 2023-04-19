43c3540059550a48803f3b1e3f59dd94022a688f
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-18 21:25:16 -0300

    Criado teste para facade com module

--- xunit/tests/test_facade.py
+++ xunit/tests/test_facade.py
@@ -1,23 +1,40 @@
 from xunit.src import *
 import xunit.src as xunit
 from xunit.tests.testclasses import DummyTestCase
+from xunit.tests import testmodule
+from xunit.tests.testclasses import MockPrint
 
 
 @TestClass
 class TestFacade(TestCase):
 
+    print: MockPrint
+
+    def setUp(self) -> None:
+        self.print = MockPrint()
+
+    @Test
+    def testMock(self) -> None:
+        self.print("Test")
+        assert self.print.passed_value == "Test"
+
     @Test
     def testFacadeWithTestClass(self) -> None:
-        from xunit.tests.testclasses import MockPrint
-        mock = MockPrint()
-        mock("Test")
-        assert mock.passed_value == "Test"
         xunit.run(
             subject=DummyTestCase,
             type="class",
-            capture_output=mock
+            capture_output=self.print
         )
-        assert mock.passed_value == self.expectedValueForClass()
+        assert self.print.passed_value == self.expectedValueForClass()
+
+    @Test
+    def testFacadeWithModule(self) -> None:
+        xunit.run(
+            subject=DummyTestCase,
+            type="module",
+            capture_output=self.print
+        )
+        assert self.print.passed_value == self.expectedValueForModule()
 
     def expectedValueForClass(self) -> str:
         result = TestResult()
@@ -27,5 +44,16 @@ class TestFacade(TestCase):
         SimpleTestSummary()
         )
         suite = TestSuite.fromTestCase(DummyTestCase)
+        suite.run(result)
+        return summary.results(result)
+
+    def expectedValueForModule(self) -> str:
+        result = TestResult()
+        summary = MixedTestSummary(
+        PassedSummary(passed_formatter=green),
+        ErrorInfoSummary(failed_formatter=red, notCompleted_formatter=yellow),
+        SimpleTestSummary()
+        )
+        suite = TestSuite.fromModule(testmodule)
         suite.run(result)
         return summary.results(result)
