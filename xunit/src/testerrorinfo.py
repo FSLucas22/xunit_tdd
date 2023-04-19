efd246a770a7fb6ab7c525a5c64fbc559dc3f62c
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-19 17:48:17 -0300

    Modificada visibilidade dos atributos test_name e error_info

--- xunit/src/testerrorinfo.py
+++ xunit/src/testerrorinfo.py
@@ -3,12 +3,10 @@ from typing import Type, Protocol
 
 
 class TestErrorInfo:
-    error_info: str
-    test_name: str
     
     def __init__(self, error_info: str, test_name: str):
-        self.error_info = error_info
-        self.test_name = test_name
+        self._error_info = error_info
+        self._test_name = test_name
 
     @staticmethod
     def fromException(error: Exception, test_name: str | None = None
@@ -19,6 +17,14 @@ class TestErrorInfo:
         )
         test_name = test_name or name
         return TestErrorInfo(error_info, test_name)
+
+    @property
+    def error_info(self) -> str:
+        return self._error_info
+
+    @property
+    def test_name(self) -> str:
+        return self._test_name
 
     def __str__(self) -> str:
         return f"TestErrorInfo({self.test_name}, {self.error_info})"
