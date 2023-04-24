1466d88aecd80384130b2803bff59e834776bc56
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-24 20:41:26 -0300

    Removido teste por se tornar obsoleto

--- xunit/tests/test_errors.py
+++ xunit/tests/test_errors.py
@@ -13,7 +13,7 @@ class TestErrors(TestCase):
         except Exception as e:
             error_info = traceback.extract_tb(e.__traceback__)[-1]
             info = TestErrorInfo.from_exception(e)
-            assert info.error_info == ''.join(
+            assert info.info == ''.join(
             traceback.format_exception(type(e), e, e.__traceback__)
             )
             assert info.test_name == "test_from_exception"
@@ -25,9 +25,3 @@ class TestErrors(TestCase):
         except Exception as e:
             info = TestErrorInfo.from_exception(e, "testMethod")
             assert info.test_name == "testMethod"
-
-    @Test
-    def test_equality(self) -> None:
-        assert TestErrorInfo("x", "y") == TestErrorInfo("x", "y")
-
-
