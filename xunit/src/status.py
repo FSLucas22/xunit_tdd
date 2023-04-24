c99074fc762b804d6ae0eb4f0635c867db149636
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-24 18:00:05 -0300

    Adaptada a classe para passar no teste

--- xunit/src/status.py
+++ xunit/src/status.py
@@ -1,18 +1,7 @@
-class TestStatus:
+from typing import NamedTuple
 
-    def __init__(self, name: str, result: str, info: str) -> None:
-        self._name = name
-        self._result = result
-        self._info = info
 
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @property
-    def result(self) -> str:
-        return self._result
-
-    @property
-    def info(self) -> str:
-        return self._info
+class TestStatus(NamedTuple):
+    name: str
+    result: str
+    info: str
