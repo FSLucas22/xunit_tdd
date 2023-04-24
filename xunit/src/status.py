3c45c70b7f379562128edf0cb3fac49170704790
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-24 17:56:44 -0300

    Refatorada classe TestStatus

--- xunit/src/status.py
+++ xunit/src/status.py
@@ -1,9 +1,18 @@
 class TestStatus:
-    name: str
-    result: str
-    info: str
 
     def __init__(self, name: str, result: str, info: str) -> None:
-        self.name = name
-        self.result = result
-        self.info = info
+        self._name = name
+        self._result = result
+        self._info = info
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def result(self) -> str:
+        return self._result
+
+    @property
+    def info(self) -> str:
+        return self._info
