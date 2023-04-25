375aeb6b3af3c296c9e58db4cc9725a5787b7be8
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-24 21:32:44 -0300

    Adaptado teste para mudanças em from_exception

--- xunit/tests/test_testcase.py
+++ xunit/tests/test_testcase.py
@@ -63,7 +63,7 @@ class TestCaseTest(TestCase):
         mock_class.run(self.result)
         assert mock_class.exception_raised == error
         expected_info = TestErrorInfo.from_exception(
-            error, test_name="testMethod"
+            error, name="testMethod"
         )
         error_info = self.result._failed_errors[0]
         assert expected_info == error_info
@@ -75,7 +75,7 @@ class TestCaseTest(TestCase):
         mock_class.run(self.result)
         assert mock_class.exception_raised == error
         expected_info = TestErrorInfo.from_exception(
-            error, test_name="testMethod"
+            error, name="testMethod"
         )
         error_info = self.result._not_completed_errors[0]
         assert error_info == expected_info
