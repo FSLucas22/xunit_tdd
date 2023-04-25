9a55fa9a697b73f0151c76a6b622def6415d9110
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-24 21:11:10 -0300

    Iniciada implementaçãod e from_exception em TestStatus

--- xunit/src/status.py
+++ xunit/src/status.py
@@ -1,7 +1,20 @@
-from typing import NamedTuple
+from typing import NamedTuple, Protocol
+import traceback
 
 
 class TestStatus(NamedTuple):
     name: str
     result: str
     info: str
+
+    @staticmethod
+    def from_exception(error: Exception, name: str, info: str) -> 'TestStatus':
+        info = ''.join(
+            traceback.format_exception(type(error), error, error.__traceback__)
+        )
+        return TestStatus(name, "Failed", info)
+
+
+class StatusFactory(Protocol):
+    def __call__(self, error: Exception, name: str, info: str) -> TestStatus:
+        pass
