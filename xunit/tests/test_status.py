39eee150adf6ed155feb2c4cb79f4602f705aafb
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-24 21:05:05 -0300

    Teste de from_exception em TestStatus

--- xunit/tests/test_status.py
+++ xunit/tests/test_status.py
@@ -1,5 +1,6 @@
 from xunit.src import *
 from xunit.src.status import TestStatus
+import traceback
 
 
 @TestClass
@@ -17,3 +18,18 @@ class TestTestStatus(TestCase):
         status1 = TestStatus("someTest", "passed", "-")
         status2 = TestStatus("someTest", "passed", "-")
         assert status1 == status2
+
+    @Test
+    def test_from_exception(self) -> None:
+        try:
+            raise InvalidAttributeException("Test")
+        except Exception as e:
+            error_info = traceback.extract_tb(e.__traceback__)[-1]
+            name = "test_from_exception"
+            result = "Not completed"
+            info = TestStatus.from_exception(e, name, result)
+            assert info.info == ''.join(
+            traceback.format_exception(type(e), e, e.__traceback__)
+            )
+            assert info.name == name
+            assert info.result == result
