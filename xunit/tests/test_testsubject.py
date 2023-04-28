34331aef849d72b925be2bcb6a9f703ea56a773f
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-27 22:23:30 -0300

    Criado teste próprio para subject e DummyObserver

---
+++ xunit/tests/test_testsubject.py
@@ -0,0 +1,37 @@
+from xunit.src import *
+from xunit.src.status import TestStatus
+from xunit.src.observer import *
+from xunit.tests.testclasses import DummyObserver
+from typing import cast
+
+
+@TestClass
+class TestSubject(TestCase):
+
+    @Test
+    def test_observer(self) -> None:
+        observer: Observer = DummyObserver()
+        status = TestStatus("x", "y", "z")
+        observer(status)
+        observer = cast(DummyObserver, observer)
+        assert observer.received == [status]
+        observer(status)
+        assert observer.received == [status, status]
+    
+    @Test
+    def test_subject(self) -> None:
+        observer1 = DummyObserver()
+        observer2 = DummyObserver()
+        subject: Subject = SubjectImp(observer1, observer2) 
+        status = TestStatus("x", "y", "z")
+        
+        subject.notify(status)
+        
+        assert observer1.received == observer2.received == [status]
+        
+        subject.unregister(observer1)
+        
+        subject.notify(status)
+        
+        assert observer1.received == [status]
+        assert observer2.received == [status, status]
