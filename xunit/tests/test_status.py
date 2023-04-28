996920dd7700cb8f714160bcbacd3d143fb23105
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-04-28 17:02:24 -0300

    Nome dos testes para a notação snake_case

--- xunit/tests/test_status.py
+++ xunit/tests/test_status.py
@@ -7,14 +7,14 @@ import traceback
 class TestTestStatus(TestCase):
 
     @Test
-    def testCreation(self) -> None:
+    def test_creation(self) -> None:
         status = TestStatus("someTest", "passed", "-")
         assert status.name == "someTest"
         assert status.result == "passed"
         assert status.info == "-"
 
     @Test
-    def testEquality(self) -> None:
+    def test_equality(self) -> None:
         status1 = TestStatus("someTest", "passed", "-")
         status2 = TestStatus("someTest", "passed", "-")
         assert status1 == status2
