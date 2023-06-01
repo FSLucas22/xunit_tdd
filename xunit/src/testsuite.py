695c64d7ba6071a69a0c49c6bbc0663e42d9a15e
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-05-31 21:02:49 -0300

    Modificado metodo

--- xunit/src/testsuite.py
+++ xunit/src/testsuite.py
@@ -1,19 +1,30 @@
-from typing import Type, Self
-from xunit.src.testcase import TestCase
-from xunit.src.status import TestStatus, Status
-from xunit.src.observer import Observer, SubjectImp
-from xunit.src.testresult import TestResult
-import xunit.src.packagemanager as pm
 from types import ModuleType
+from typing import Self, Type, Protocol
+
+import xunit.src.packagemanager as pm
+from xunit.src.observer import Observer, Subject, SubjectImp
+from xunit.src.status import Status, StatusFactory, TestStatus
+from xunit.src.testcase import TestCase
+
+
+class Runnable(Subject, Protocol):
+
+    def run(self) -> None:
+        ...
 
 
 class TestSuite(SubjectImp):
-    
-    def __init__(self, *observers: Observer) -> None:
-        self._tests: list[TestCase] = []
+    name: str = "suite"
+    erro_info_factory: StatusFactory = TestStatus.from_exception
+
+    def __init__(self, *observers: Observer, name: str | None = None, 
+                 error_info_factory: StatusFactory | None = None) -> None:
+        self._tests: list[Runnable] = []
+        self.name = name or type(self).name
+        self.error_info_factory = error_info_factory or type(self).erro_info_factory
         super().__init__(*observers)
         
-    def add(self, *tests: TestCase) -> None:
+    def add(self, *tests: Runnable) -> None:
         for test in tests:
             test.register(self.notify)
         self._tests += list(tests)
@@ -27,23 +38,34 @@ class TestSuite(SubjectImp):
         return merged
 
     def run(self) -> None:
-        self.notify(TestStatus("Suite", Status.CREATED, "individual"))
+        self.notify(TestStatus("Suite", Status.CREATED, self.name))
         for test in self._tests:
-            test.run()
+            try:
+                test.run()
+            except Exception as e:
+                print(self.error_info_factory)
+                self.notify(
+                    self.error_info_factory(e,
+                    type(test).__name__, Status.FAILED_TO_RUN)
+                )
 
     @classmethod
-    def suite(cls, observers: list[Observer] | None = None) -> Self:
+    def suite(cls, observers: list[Observer] | None = None, name: str | None = None,
+              error_info_factory: StatusFactory | None = None) -> Self:
         if observers is None:
             observers = []
-        return cls(*observers)
+        return cls(*observers, name=name, error_info_factory=error_info_factory)
 
     @classmethod
     def from_test_case(cls, *tests: Type[TestCase],
-                       observers: list[Observer] | None = None) -> Self:
-        suite = cls.suite(observers)
+                       observers: list[Observer] | None = None, name: str | None = None,
+                       error_info_factory: StatusFactory | None = None) -> Self:
+        suite = cls.suite(observers, name, error_info_factory)
         for test_case in tests:
+            test_suite = cls.suite(name=test_case.__name__, error_info_factory=error_info_factory)
             for testname in test_case.xunit_test_names.split():
-                suite.add(test_case(testname))
+                test_suite.add(test_case(testname))
+            suite.add(test_suite)
         return suite
 
     @classmethod
