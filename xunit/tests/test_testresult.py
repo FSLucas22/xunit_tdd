890a51d9c5baee53bdf01002ea9ff6b822f97cb7
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-06-06 23:33:36 -0300

    Removidos testes redundantes

--- xunit/tests/test_testresult.py
+++ xunit/tests/test_testresult.py
@@ -27,42 +27,10 @@ class TestResultTest(TestCase):
         assert "failedTest" == self.result.get_names_of_status(Status.FAILED)
 
     @Test
-    def test_completed_tests(self) -> None:
-        self.result.save_status(TestStatus("someTest", Status.FAILED, ""))
-        assert self.result.get_names_of_status(Status.FAILED) == "someTest"
-
-    @Test
     def test_completed_multiple_tests(self) -> None:
         self.result.save_status(TestStatus("someTest", Status.FAILED, ""))
         self.result.save_status(TestStatus("someOtherTest", Status.FAILED, ""))
         assert self.result.get_names_of_status(Status.FAILED) == "someTest someOtherTest"
-
-    @Test
-    def test_not_completed_tests(self) -> None:
-        assert self.result.get_names_of_status(Status.NOT_COMPLETED) == ""
-        self.result.save_status(TestStatus("someBrokenTest", Status.NOT_COMPLETED, ""))
-        assert self.result.get_names_of_status(Status.NOT_COMPLETED) == "someBrokenTest"
-        self.result.save_status(TestStatus("someOtherBrokenTest", Status.NOT_COMPLETED, ""))
-        assert self.result.get_names_of_status(Status.NOT_COMPLETED) == "someBrokenTest someOtherBrokenTest"
-
-    @Test
-    def test_passed_tests(self) -> None:
-        assert self.result.get_names_of_status(Status.PASSED) == ""
-        self.result.save_status(TestStatus("someTest", Status.PASSED, ""))
-        self.result.save_status(TestStatus("someOtherTest", Status.PASSED, ""))
-        assert self.result.get_names_of_status(Status.PASSED) == "someTest someOtherTest"
-
-    @Test
-    def test_runned_equals_passed_plus_failed(self) -> None:
-        suite = TestSuite()
-        suite.add(WasRun("testMethod"))
-        suite.add(WasRun("testBrokenMethod"))
-        suite.add(FailedSetUp("testMethod"))
-        suite.register(self.result.save_status)
-        suite.run()
-        
-        assert self.result.get_status_count(Status.PASSED) == 1
-        assert self.result.get_status_count(Status.FAILED) == 1
 
     @Test
     def test_save_status(self) -> None:
@@ -72,19 +40,3 @@ class TestResultTest(TestCase):
         assert self.result.get_names_of_status(Status.PASSED) == "someTest"
         assert self.result.get_names_of_status(Status.FAILED) == "someOtherTest"
         assert self.result.get_names_of_status(Status.NOT_COMPLETED) == "brokenTest"
-        
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
